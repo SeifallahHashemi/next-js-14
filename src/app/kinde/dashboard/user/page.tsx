c55defd418b1a4a4cc3da2ca265cf2c6e4d98941
SEPEHR HASHEMI <sepehrhashemimamaquani1996@gmail.com>
2024-01-03 11:52:24 +0330

    nextjs_image_blur_effect

---
+++ src/app/kinde/dashboard/user/page.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
+
+const KindeDashboard = async () => {
+  const { isAuthenticated, getPermission } = getKindeServerSession();
+  const isAuthed = await isAuthenticated();
+  if (!isAuthed) {
+    redirect('/api/auth/login');
+  }
+  const requiredPermission = await getPermission('user:access');
+  if (!requiredPermission?.isGranted) {
+    redirect('/')
+  }
+  return (
+    <div className='relative max-w-2xl mx-auto'>
+      <input
+        type='email'
+        className='peer block w-full rounded-lg border-transparent bg-gray-100 px-4 py-3 ps-11 text-sm focus:border-blue-500 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 dark:border-transparent dark:bg-gray-700 dark:text-gray-400 dark:focus:ring-gray-600'
+        placeholder='Enter name'
+      />
+      <div className='pointer-events-none absolute inset-y-0 start-0 flex items-center ps-4 peer-disabled:pointer-events-none peer-disabled:opacity-50'>
+        <svg
+          className='h-4 w-4 flex-shrink-0 text-gray-500'
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='none'
+          stroke='currentColor'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <path d='M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2' />
+          <circle cx='12' cy='7' r='4' />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default KindeDashboard;
---
+++ src/components/NextJsBlurImage/NextJsBlurImage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+import ImageSrc from "@/../public/images/pic - (10).jpg"
+import { getPlaiceholder } from "plaiceholder";
+
+const NextJsBlurImage = async () => {
+  const imageUrl = "https://images.unsplash.com/photo-1702325250074-28bc0f34646e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  const buffer = await fetch(imageUrl).then(async (res) => Buffer.from(await res.arrayBuffer()));
+  const { base64 } = await getPlaiceholder(buffer);
+  return (
+    <section className={"grid place-content-center grid-cols-2"}>
+      <div className={"w-96"}><Image src={ImageSrc} alt={"Static Image"} className={"max-w-full h-auto"} placeholder={"blur"} sizes={"(max-width: 768px) 100vw, 33vw"}/></div>
+      <div className={"w-96"}><Image src={imageUrl} alt={"Static Image"} className={"max-w-full h-auto"} placeholder={"blur"} sizes={"(max-width: 768px) 100vw, 33vw"} blurDataURL={base64} width={640} height={640}/></div>
+    </section>
+  );
+};
+
+export default NextJsBlurImage;
